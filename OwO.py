d81786642b8edbc19be3f050d4249613bfcf37c5
Chai <83523587+CPScript@users.noreply.github.com>
2023-05-10 10:06:25 -0400

    Update OwO.py

--- OwO.py
+++ OwO.py
@@ -6,3 +6,11 @@ while True:
     sleep(0.05)
     if keyboard.is_pressed("l") or keyboard.is_pressed("r"):
         keyboard.write("\bw")
+    
+    if keyboard.is_pressed("!") or keyboard.is_preseed("?"):
+        keyboard.write("\b UwU")
+   
+    if keyboard.is_pressed(".")
+        keyboard.write("\b. V-V")
+        
+# Idek... lol
